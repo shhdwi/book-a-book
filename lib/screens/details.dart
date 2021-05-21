2058477a66dfc9f580f499f2f5e4cd0342cad2df
SoulReaper2408 <gaurangaswal2408@gmail.com>
2021-05-22 01:01:39 +0530

    Widgets for details page

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -10,55 +10,97 @@ class ProductDetails extends StatelessWidget {
     ProductNotifier productNotifier = Provider.of<ProductNotifier>(
         context, listen: false);
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-            vertical: 5.0, horizontal: 5.0),
-        child: ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: Stack(
-              children: [
-                Image.network(productNotifier.currentData.imgUrl, fit: BoxFit.fill,),
-                Positioned.fill(
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
-                    child: Container(color: Colors.orangeAccent.withOpacity(0.1)),
 
 
-                  ),
-                ),
-                Positioned.fill(
-
-                  child: Padding(
-
-                    padding: const EdgeInsets.all(60),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25)),
-
-                          child: Card(
-
-
-
-
-                            child :Hero(tag:productNotifier.currentData.imgUrl,child: Image.network(productNotifier.currentData.imgUrl, fit: BoxFit.cover)),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              vertical: 5.0, horizontal: 5.0),
+          child: Column(
+            children: [
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Stack(
+                    children: [
+                      Image.network(productNotifier.currentData.imgUrl, fit: BoxFit.fill,),
+                      Positioned.fill(
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
+                          child: Container(color: Colors.orangeAccent.withOpacity(0.1)),
 
 
-
-                          ),
                         ),
                       ),
+                      Positioned.fill(
+
+                        child: Padding(
+
+                          padding: const EdgeInsets.all(60),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(25)),
+
+                                child: Card(
+
+
+
+
+                                  child :Hero(tag:productNotifier.currentData.imgUrl,child: Image.network(productNotifier.currentData.imgUrl, fit: BoxFit.cover)),
+
+
+
+                                ),
+                              ),
+                            ),
+                        ),
+
+                      ])
+
+
+
+
                   ),
 
-                ])
 
 
-
-
+           Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(''),// Book name
+                Text(''), // Author
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                Container(
+            color: Colors.amber,
+              padding: EdgeInsets.all(30.0),
+              child: Text('Number of Pages'),
             ),
+            Container(
+            color: Colors.amber,
+            padding: EdgeInsets.all(30.0),
+            child: Text('Subject'),
+          ),
+
+  ]
+                ),
+                Text('Description'),
+                RaisedButton(
+                  padding: EdgeInsets.symmetric(vertical:15,horizontal:45),
+                  onPressed: () {},
+                  color: Colors.deepPurpleAccent,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))) ,
+                  child: Text('Contact Details')
+                ),
+          ],
+
+    ),
+        ],
+
+    )
+        ),
       ),
 
 
