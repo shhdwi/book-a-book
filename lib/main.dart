a0a9832bf40955a9e9f24b523e8b993986619639
SoulReaper2408 <gaurangaswal2408@gmail.com>
2021-05-25 18:51:15 +0530

    Splash Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
 import 'package:catalog_app/helper_functions/Authentication.dart';
-import 'package:catalog_app/screens/add_product.dart';
 import 'package:catalog_app/screens/display_page.dart';
 import 'package:catalog_app/screens/home_page.dart';
-import 'package:catalog_app/screens/login_page.dart';
-import 'package:catalog_app/screens/products_page.dart';
-import 'package:catalog_app/screens/signup.dart';
-import 'package:catalog_app/services/auth.dart';
+import 'package:catalog_app/screens/splash_screen.dart';
 import 'package:catalog_app/services/product_notifier.dart';
 import 'package:catalog_app/utils/routes.dart';
-import 'package:catalog_app/widgets/nav.dart';
 import 'package:catalog_app/widgets/themes.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,16 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FutureBuilder(
-        future: AuthMethods().getCurrentUser(),
-        builder: (context , AsyncSnapshot<dynamic> snapshot){
-          if(snapshot.hasData){
-            return MyHomePage();
-          }else{
-            return Authenticate();
-          }
-        },
-      ),
+      home: Splash(),
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
 
@@ -53,3 +40,5 @@ class MyApp extends StatelessWidget {
         );
   }
 }
+
+
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,50 @@
+import'dart:async';
+import 'package:catalog_app/helper_functions/Authentication.dart';
+import 'package:catalog_app/services/auth.dart';
+import 'package:catalog_app/widgets/nav.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+class Splash extends StatefulWidget {
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 4),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) => Screen()
+            )
+        )
+    );
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Lottie.asset('assets/lottie/splash.json')
+        )
+    );
+  }
+}
+class Screen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FutureBuilder(
+        future: AuthMethods().getCurrentUser(),
+        builder: (context , AsyncSnapshot<dynamic> snapshot){
+          if(snapshot.hasData){
+            return MyHomePage();
+          }else{
+            return Authenticate();
+          }
+        },
+      ),
+    );
+  }
+}
